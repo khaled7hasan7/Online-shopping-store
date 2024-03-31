8d75aff0ba61e9203d176d2bee71b22f041c6c36
khaled hasan <131875788+khaled7hasan7@users.noreply.github.com>
2024-04-01 00:36:10 +0200

    Update proj.sql

--- proj.sql
+++ proj.sql
@@ -48,19 +48,6 @@ CREATE TABLE `customers` (
 --
 -- Dumping data for table `customers`
 --
-
-INSERT INTO `customers` (`name`, `flat`, `street`, `city`, `country`, `birth`, `id`, `email`, `telephone`, `cardnumber`, `expirationdate`, `cardname`, `bankissued`, `username`, `password`) VALUES
-('sara ahmad', 0, 11111, 'ramallah', 'Palestine', '2009-02-03', 123456789, 'siamlayali5@gmail.com', 522478418, 2232, 'da', 'pls', 'bankk', '0123456', '12345678'),
-('khaled', 0, 2254, 'ramallah', 'Palestine', '2024-02-07', 2147483647, 'kh2662174@gmail.com', 522478418, 1, '10', '10', '10', '012345678', '012345678'),
-('translation', 0, 2254, 'ramallah', 'Palestine', '2024-01-10', 2147483647, 'siamlayali5@gmail.com', 522478418, 1234, '132', 'pls', 'bankk', '1191494', '12345678'),
-('translation', 0, 2254, 'ramallah', 'Palestine', '2024-01-01', 25151515, '1191494@student.birzeit.edu', 522478418, 564, 'l njkb', 'njm k', '24', '12345678', '12345678'),
-('omar ahmad', 0, 2254, 'ramallah', 'Palestine', '2024-01-04', 2147483647, '1191494@student.birzeit.edu', 522478418, 5656, 'lasnd', 'pls', 'bankk', '55555555', '55555555'),
-('khaled', 0, 11111, 'ramallah', 'Palestine', '2024-02-07', 214587, 'khaled7hasan@gmail.com', 522478418, 0, 'sad', 'asd', 'asd', '88888888', '88888888'),
-('omar khaled', 0, 11111, 'ramallah', 'Palestine', '2024-02-03', 42262015, '1191494@student.birzeit.edu', 522478418, 333334, 'ads', 'as', 'asd', '987654321', '987654321'),
-('ahmad omar', 0, 11111, 'ramallah', 'Palestine', '2024-01-10', 123456789, 'kh2662174@gmail.com', 522478418, 1123, 'sdfs', '234 d', 'bankk', 'kh2662174', '12345678'),
-('translation', 0, 2254, 'ramallah', 'Palestine', '2024-01-03', 2147483647, 'siamlayali5@gmail.com', 522478418, 5656, 'KLLJKJK', '32c2', 'cas43', 'khaled555', 'KHALED555'),
-('ahmad', 0, 666, 'ramallah', 'Palestine', '2024-02-06', 2147483647, 'kh2662174@gmail.com', 522478418, 1212, '1212', 'njm k', 'bankk', 'khaled78', 'khaled78');
-
 -- --------------------------------------------------------
 
 --
@@ -76,9 +63,6 @@ CREATE TABLE `employees` (
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`username`, `password`) VALUES
-('employee', 'employee'),
-('khaledemp', 'khaledemp');
 
 -- --------------------------------------------------------
 
@@ -102,29 +86,6 @@ CREATE TABLE `orders` (
 --
 -- Dumping data for table `orders`
 --
-
-INSERT INTO `orders` (`order_id`, `username`, `product_id`, `product_name`, `quantity`, `product_price`, `product_image`, `state`, `date`, `prs_id`) VALUES
-(82, '12345678', '1000000000', 'Virgin Olive Oil', 1, 30, 'item1000000000img1.jpg', 'shipped', '2024-02-01 10:50:07 PM', 1706824207),
-(83, '12345678', '0000000000', 'Pottery', 1, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-01 10:50:19 PM', 1706824219),
-(84, '12345678', '1000000000', 'Virgin Olive Oil', 3, 30, 'item1000000000img1.jpg', 'shipped', '2024-02-01 11:01:50 PM', 1706824910),
-(85, '12345678', '1000000002', 'ZAATER JENIN', 1, 10, 'item1000000002img1.png', 'shipped', '2024-02-01 11:01:50 PM', 1706824910),
-(86, '12345678', '1706756971', 'Olive Oil Nabulsi Soap', 5, 4, 'item1706756971img1.png', 'shipped', '2024-02-01 11:01:50 PM', 1706824910),
-(87, '12345678', '0000000000', 'Pottery', 1, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-01 11:28:23 PM', 1706826503),
-(88, '12345678', '0000000000', 'Pottery', 1, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-01 11:28:23 PM', 1706826503),
-(89, '12345678', '1706756971', 'Olive Oil Nabulsi Soap', 4, 4, 'item1706756971img1.png', 'shipped', '2024-02-01 11:28:23 PM', 1706826503),
-(90, 'khaled78', '1000000002', 'ZAATER JENIN', 1, 10, 'item1000000002img1.png', 'shipped', '2024-02-01 11:37:31 PM', 1706827051),
-(91, 'khaled78', '1000000005', 'Palestinian Green Zaater', 4, 3, 'item1000000005img1.png', 'shipped', '2024-02-01 11:37:31 PM', 1706827051),
-(92, 'khaled78', '0000000000', 'Pottery', 4, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-01 11:37:31 PM', 1706827051),
-(94, 'khaled78', '1000000000', 'Virgin Olive Oil', 1, 30, 'item1000000000img1.jpg', 'shipped', '2024-02-02 01:17:06 PM', 1706876226),
-(95, 'khaled78', '0000000000', 'Pottery', 3, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-02 01:17:06 PM', 1706876226),
-(96, '12345678', '1000000000', 'Virgin Olive Oil', 1, 30, 'item1000000000img1.jpg', 'shipped', '2024-02-02 04:18:39 PM', 1706887119),
-(98, '12345678', '1000000005', 'Palestinian Green Zaater', 0, 3, 'item1000000005img1.png', 'shipped', '2024-02-02 04:18:39 PM', 1706887119),
-(99, '12345678', '1000000000', 'Virgin Olive Oil', 1, 30, 'item1000000000img1.jpg', 'shipped', '2024-02-02 04:41:26 PM', 1706888486),
-(100, '12345678', '0000000000', 'Pottery', 3, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-02 04:41:26 PM', 1706888486),
-(101, '12345678', '1000000002', 'ZAATER JENIN', 4, 10, 'item1000000002img1.png', 'shipped', '2024-02-02 04:41:26 PM', 1706888486),
-(102, '12345678', '0000000000', 'Pottery', 3, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-02 06:34:47 PM', 1706895287),
-(103, '12345678', '0000000000', 'Pottery', 14, 100, 'item0000000000img1.jpg', 'shipped', '2024-02-02 06:34:47 PM', 1706895287),
-(104, '12345678', '1000000000', 'Virgin Olive Oil', 1, 30, 'item1000000000img1.jpg', 'basket', '2024-02-02 06:57:16 PM', 0);
 
 -- --------------------------------------------------------
 
@@ -176,18 +137,6 @@ CREATE TABLE `prossing` (
 --
 -- Dumping data for table `prossing`
 --
-
-INSERT INTO `prossing` (`prs_id`, `username`, `date`) VALUES
-(1706824207, '12345678', '2024-02-01 10:50:07'),
-(1706824219, '12345678', '2024-02-01 10:50:19'),
-(1706824910, '12345678', '2024-02-01 11:01:50'),
-(1706826503, '12345678', '2024-02-01 11:28:23'),
-(1706827051, 'khaled78', '2024-02-01 11:37:31'),
-(1706876226, 'khaled78', '2024-02-02 01:17:06'),
-(1706887100, 'employee', '2024-02-02 04:18:20'),
-(1706887119, '12345678', '2024-02-02 04:18:39'),
-(1706888486, '12345678', '2024-02-02 04:41:26'),
-(1706895287, '12345678', '2024-02-02 06:34:47');
 
 --
 -- Indexes for dumped tables
